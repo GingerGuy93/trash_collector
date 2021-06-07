89eab3a305d87a04223e1965dc0ee9c728167158
Your Name <you@example.com>
2021-06-07 11:35:15 -0500

    added create employee to views and templates

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
-# Create your models here.
 
-# TODO: Create an Employee model with properties required by the user stories
+# Create your models here.
 
 
 class Employees(models.Model):
     name = models.CharField(max_length=50)
-    zip_code = models.CharField(max_length=10)
+    zip_code = models.CharField(max_length=10, blank=True, null=True)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.apps import apps
-
+from .models import Employees
+from datetime import date
+from django.urls import reverse
 # Create your views here.
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
@@ -11,3 +13,17 @@ def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db
     Customer = apps.get_model('customers.Customer')
     return render(request, 'employees/index.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        print(request)
+        user = request.user
+        name = request.POST.get('name')
+        zip_code = request.POST.get('zip_code')
+        new_employee = Employees(name=name, zip_code=zip_code)
+        new_employee.user_id = user.id
+        new_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        return render(request, 'employees/create.html')
