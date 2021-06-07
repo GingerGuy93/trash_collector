1c6bbafbe2752ed08dd6d755b10decc3d51d3383
Your Name <you@example.com>
2021-06-07 17:30:03 -0500

    added confirmation of pick up to employees

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -4,6 +4,8 @@ from django.apps import apps
 from .models import Employees
 from datetime import date
 from django.urls import reverse
+
+
 # Create your views here.
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
@@ -17,13 +19,22 @@ def index(request):
 
 def create(request):
     if request.method == 'POST':
-        print(request)
         user = request.user
         name = request.POST.get('name')
         zip_code = request.POST.get('zip_code')
-        new_employee = Employees(name=name, zip_code=zip_code)
-        new_employee.user_id = user.id
+        new_employee = Employees(name=name, zip_code=zip_code, user=user)
         new_employee.save()
         return HttpResponseRedirect(reverse('employees:index'))
     else:
         return render(request, 'employees/create.html')
+
+
+def confirm_pickup(request, customer_id):
+    Customer = apps.get_model('customers.Customer')
+    customer = Customer.objects.get(pk=customer_id)
+    customer.account_balance += 5
+    customer.save()
+    context = {
+        'customer': customer
+    }
+    return render(request, 'employees/confirm.html', context)
